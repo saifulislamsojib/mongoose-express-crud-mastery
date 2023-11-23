0d7ac2682bacb7a85e8e6e12c75c4313f7cca41f
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-11-24 00:53:11 +0600

    user route added

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -2,4 +2,13 @@ import { Router } from 'express';
 
 const userRoutes = Router();
 
+userRoutes.post('/');
+userRoutes.get('/');
+userRoutes.get('/:userId/orders/total-price');
+userRoutes.get('/:userId/orders');
+userRoutes.get('/:userId');
+userRoutes.put('/:userId/orders');
+userRoutes.put('/:userId');
+userRoutes.delete('/:userId');
+
 export default userRoutes;
